2739582f43bae1acb2278364c154b10a378e4655
miked9372 <57244859+miked9372@users.noreply.github.com>
2022-08-25 16:38:23 -0500

    Update psu_mn_data.py

--- psu_mn_data.py
+++ psu_mn_data.py
@@ -1,5 +1,5 @@
 [
-{raw_data : {'status': {'cd': 0, 'mg': 'successful', 'tp': 'A'},
+{raw_data  {'status': {'cd': 0, 'mg': 'successful', 'tp': 'A'},
             'value': {
             L:101: {so: 0, pl: [{cd: "9", cn: 202, tc: 557}]},
             L:103: {so: 0, pl: [{cd: "9", cn: 194, tc: 519}]},
